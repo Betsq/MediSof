b57b3f9e58a27a1520e431edb60dcbaff120874d
betsq9 <betsq9@gmail.com>
2023-08-24 13:06:30 +0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ContactUsPopUpComponent } from './core/components/contact-us-pop-up/contact-us-pop-up.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
+
 })
 export class AppComponent {
   constructor(public dialog: MatDialog) {}
-  
+
   title = 'Medi Sof';
 
   openPopUp(){
     const dialogRef = this.dialog.open(ContactUsPopUpComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
-    
-    });
+    dialogRef.afterClosed().subscribe();
   }
 }
