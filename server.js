267e82e196e76da3b837dfeb0f335ea96b46b31f
Betsq <betsq9@gmail.com>
2023-09-25 18:59:35 +0300

    Update

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/medi-sof'));
+
+app.use(express.static('/home/site/wwwroot'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/medi-sof/index.html'));});
+res.sendFile(path.join('/home/site/wwwroot/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
