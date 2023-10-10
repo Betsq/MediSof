3a41f10171111d290a2e4797810976b38dbcd2e1
Betsq <betsq9@gmail.com>
2023-10-10 23:21:12 +0300

    Upd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/MediSof'));
+app.use(express.static(__dirname + '/dist/medi-sof'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/MediSof/index.html'));});
+res.sendFile(path.join(__dirname + '/dist/medi-sof/index.html'));});
 app.listen(process.env.PORT || 8080);
