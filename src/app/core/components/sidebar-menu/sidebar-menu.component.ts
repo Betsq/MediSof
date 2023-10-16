9219a363fbcb4fb2569f15a854a001022b21c6a6
Betsq <betsq9@gmail.com>
2023-10-16 15:57:43 +0300

    pdat

--- src/app/core/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/core/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,5 +1,11 @@
-import {Component } from "@angular/core"
+import {Component, ViewChild } from "@angular/core"
 import * as SharedInformation from "../../../shared/shared-information.json";
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, filter, withLatestFrom } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
+import { MatSidenav } from "@angular/material/sidenav";
+
 
 @Component({
     selector: 'sidebar-menu',
@@ -9,4 +15,20 @@ import * as SharedInformation from "../../../shared/shared-information.json";
 
 export class SidebarMenuComponent{
     sharedInformation: any = SharedInformation;
+
+    @ViewChild('drawer')
+    drawer!: MatSidenav;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver,
+    router: Router) {
+    router.events.pipe(
+      withLatestFrom(this.isHandset$),
+      filter(([a, b]) => b && a instanceof NavigationEnd)
+    ).subscribe(_ => this.drawer.close());
+  }
 }
